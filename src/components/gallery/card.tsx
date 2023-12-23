fe52a4c67dd6372f066adf7444002c76d726f1fa
sid <hi@sid.fyi>
2023-12-22 22:50:27 -0800

    export photo

--- src/components/gallery/card.tsx
+++ src/components/gallery/card.tsx
@@ -6,4 +6,5 @@ const Card: React.FC<Photo> = function ({ w, h, id }) {
   return <Image alt="" height={640 * (h / w)} sizes="640w" src={`https://cdn.sid.fyi/photos/${id}.jpeg`} width={640} />;
 };
 
+export type { Photo };
 export default Card;
