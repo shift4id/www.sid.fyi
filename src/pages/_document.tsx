c448b13f37c09a74dff44806ef91f88eac69f3e1
sid <hi@sid.fyi>
2024-01-03 23:24:08 -0800

    new favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,17 +3,17 @@ import { Head, Html, Main, NextScript } from "next/document";
 const Document: React.FC = () => (
   <Html className="antialiased" lang="en">
     <Head>
-      <link href="/favicons/site.webmanifest" rel="manifest" />
-
-      <link href="/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
-      <link href="/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-      <link href="/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-      <link color="#333" href="/favicons/safari-pinned-tab.svg" rel="mask-icon" />
-      <link href="/favicon.ico" rel="shortcut icon" />
-
-      <meta content="#333" name="msapplication-TileColor" />
-      <meta content="/favicons/browserconfig.xml" name="msapplication-config" />
-      <meta content="#333" name="theme-color" />
+      <link href="/favicons/apple-touch-icon.png?v=2024" rel="apple-touch-icon" sizes="180x180" />
+      <link href="/favicons/favicon-32x32.png?v=2024" rel="icon" sizes="32x32" type="image/png" />
+      <link href="/favicons/favicon-16x16.png?v=2024" rel="icon" sizes="16x16" type="image/png" />
+      <link href="/favicons/site.webmanifest?v=2024" rel="manifest" />
+      <link color="#ff80ff" href="/favicons/safari-pinned-tab.svg?v=2024" rel="mask-icon" />
+      <link href="/favicon.ico?v=2024" rel="shortcut icon" />
+      <meta content="sid.fyi" name="apple-mobile-web-app-title" />
+      <meta content="sid.fyi" name="application-name" />
+      <meta content="#ff80ff" name="msapplication-TileColor" />
+      <meta content="/favicons/browserconfig.xml?v=2024" name="msapplication-config" />
+      <meta content="#ff80ff" name="theme-color" />
     </Head>
     <body className="mx-auto max-w-3xl scroll-smooth bg-white text-black selection:bg-yellow">
       <Main />
