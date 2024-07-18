162a38c5a28857be4081efd5e68c6ca9e2786ef9
sid <hi@sid.fyi>
2024-07-18 02:16:35 -0700

    small improvement

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ export { Container } from "./container";
 export { Footer } from "./footer";
 export { Grid } from "./grid";
 export { Link } from "./link";
-export { NavBar } from "./navigation";
+export { NavBar } from "./nav-bar";
 export { Section } from "./section";
 export { Skeleton } from "./skeleton";
