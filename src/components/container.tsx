3248da190481790aa27b085d75ffca3be7be99e2
sid <hi@sid.fyi>
2024-07-18 02:26:51 -0700

    more ui clean up

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,17 +5,15 @@ type ContainerProps = React.PropsWithChildren<{
 
 function Container({ title, description, children }: ContainerProps): React.ReactNode {
   return (
-    <main className="mx-auto w-full max-w-3xl grow py-20">
-      <div className="w-full space-y-8">
-        <h1 className="text-3xl !leading-normal md:text-4xl">
-          <em className="bg-accent/20 dark:bg-accent/15">{title}</em>
-        </h1>
-        <h2 className="ml-auto max-w-lg text-right text-2xl !leading-normal text-muted md:text-3xl">
-          {description}
-        </h2>
-        <hr className="border-subtle" />
-        {children}
-      </div>
+    <main className="w-full grow space-y-8 py-20">
+      <h1 className="text-3xl !leading-normal md:text-4xl">
+        <em className="bg-accent/20 dark:bg-accent/15">{title}</em>
+      </h1>
+      <h2 className="ml-auto max-w-lg text-right text-2xl !leading-normal text-muted md:text-3xl">
+        {description}
+      </h2>
+      <hr className="border-subtle" />
+      {children}
     </main>
   );
 }
