329c569e419e9bb5240b21d5255cb529380b45af
sid <hi@sid.fyi>
2024-07-26 00:22:39 -0700

    remove it

--- src/app/components/hobby.tsx
+++ src/app/components/hobby.tsx
@@ -8,10 +8,7 @@ interface HobbyProps {
 function Hobby({ title, href }: HobbyProps): React.ReactNode {
   return (
     <Link className="group flex items-center py-1" href={href}>
-      <div
-        aria-hidden
-        className="mr-4 select-none text-muted transition-transform group-hover:translate-x-1 group-focus-visible:translate-x-1"
-      >
+      <div className="mr-4 select-none text-muted transition-transform group-hover:translate-x-1 group-focus-visible:translate-x-1">
         &rarr;
       </div>
       {title}
