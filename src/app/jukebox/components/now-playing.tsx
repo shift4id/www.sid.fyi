6092257c3ccc1d419192052ea74f57c86bb664c8
sid <hi@sid.fyi>
2024-08-06 19:51:06 -0700

    fix for revalidation

--- src/app/jukebox/components/now-playing.tsx
+++ src/app/jukebox/components/now-playing.tsx
@@ -7,8 +7,15 @@ import { Item } from "./item";
 const defaultSong: Song = { name: "Not Playing", artist: "N/A", url: "https://spotify.com", type: "song" };
 
 const useNowPlaying = (): Song | Record<string, never> => {
-  const { data: song, isLoading } = useSWR<Song | null>("/api/spotify/now-playing", (url: string) =>
-    fetch(url).then((r) => r.json()),
+  const { data: song, isLoading } = useSWR<Song | null>(
+    "/api/spotify/now-playing",
+    (url: string) => fetch(url, { next: { revalidate: 60 } }).then((r) => r.json()),
+    {
+      refreshInterval: 60 * 1000,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      revalidateIfStale: false,
+    },
   );
 
   return isLoading ? {} : (song ?? defaultSong);
