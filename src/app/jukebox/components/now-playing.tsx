667b6707c333a35de9aff49773977ab5be7e7f6a
sid <hi@sid.fyi>
2024-10-30 21:10:00 -0700

    fix: switch to fetch

--- src/app/jukebox/components/now-playing.tsx
+++ src/app/jukebox/components/now-playing.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import axios from "axios";
 import { Suspense, use, useMemo } from "react";
 import type { Song } from "@/lib/spotify";
 import { Item } from "./item";
@@ -8,7 +7,9 @@ import { Item } from "./item";
 export const defaultSong: Song = { name: "Silence", artist: "N/A", url: "https://spotify.com", type: "song" };
 
 export const fetchNowPlaying = async (): Promise<Song> => {
-  const song = await axios.get<Song | null>("/api/now-playing").then((r) => r.data);
+  const song = await fetch(`${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/now-playing`)
+    .then((r) => r.json() as Promise<Song | null>)
+    .catch(() => null);
 
   return song ?? defaultSong;
 };
