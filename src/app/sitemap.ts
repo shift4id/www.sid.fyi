39e44ff80d615fabddfe922cc30b3884a7f7978b
sid <hi@sid.fyi>
2025-03-13 22:36:25 -0700

    fix: env import

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,34 @@
 import type { MetadataRoute } from "next";
-import { client } from "@/constants/env";
+import { clientEnv } from "@/constants/env";
 
 function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: client.WEBSITE_URL,
+      url: clientEnv.WEBSITE_URL,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: `${client.WEBSITE_URL}/chat`,
+      url: `${clientEnv.WEBSITE_URL}/chat`,
       lastModified: new Date(),
       changeFrequency: "never",
       priority: 0.8,
     },
     {
-      url: `${client.WEBSITE_URL}/gallery`,
+      url: `${clientEnv.WEBSITE_URL}/gallery`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
     },
     {
-      url: `${client.WEBSITE_URL}/jukebox`,
+      url: `${clientEnv.WEBSITE_URL}/jukebox`,
       lastModified: new Date(),
       changeFrequency: "hourly",
       priority: 0.8,
     },
     {
-      url: `${client.WEBSITE_URL}/library`,
+      url: `${clientEnv.WEBSITE_URL}/library`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
