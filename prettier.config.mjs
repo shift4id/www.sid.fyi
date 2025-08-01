bb4cb02c348a4c25364a2be73f2790929732056a
sid <hi@sid.fyi>
2025-08-01 16:11:13 -0700

    fix: update prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,9 +2,9 @@
 
 /** @type {import('prettier').Config} */
 const config = {
-  printWidth: 110,
+  printWidth: 100,
   tailwindFunctions: ["cn"],
-  plugins: ["prettier-plugin-packagejson", "prettier-plugin-tailwindcss"],
+  plugins: [ "prettier-plugin-tailwindcss"],
 };
 
 export default config;
