7855fa2e0157523e76888fb85080c3108ddff40b
sid adusumelli <git@sid.fyi>
2026-03-09 16:49:41 -0700

    chore: clean deps

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -8,7 +8,6 @@ export const clientEnv = {
 export const serverEnv = {
   NOTION_API_KEY: process.env.NOTION_API_KEY,
   BOOK_DATA_SOURCE_ID: process.env.BOOK_DATA_SOURCE_ID,
-
   SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
   SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
   SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
